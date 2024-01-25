c33534071b977b5fc9df0d50dd6b86f1101e4445
acabalt <cabaltrejos@gmail.com>
2024-01-25 17:50:13 -0600

    Inicialization StoreWeb

---
+++ StoreAI/src/app/modules/shared/layouts/main/main.layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './main.layout.html',
+  styleUrl: './main.layout.css'
+})
+export class MainLayout {
+
+}
