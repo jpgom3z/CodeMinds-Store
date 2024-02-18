61b1b29a889d0b963a614c3b9810976f4b81e650
jpgom3z <jpgom29@gmail.com>
2024-02-18 01:33:40 -0600

    Adding themes, HTML & CSS for main layout, DarkMode functionality working as expected. Main Layout still missing price range filter.

--- StoreWeb/src/app/app.component.ts
+++ StoreWeb/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutService } from './services/layout/layout.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'StoreWeb';
+export class AppComponent implements OnInit {
+  constructor(
+    private layoutService: LayoutService
+  ){}
+
+
+  public  ngOnInit(): void {
+    this.layoutService.themeSubject.subscribe((theme) => {
+      document.body.className = theme;
+    });
+  }
 }
